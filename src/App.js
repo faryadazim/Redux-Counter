0c9bac0bfc40ccfb3238d62efa3910b845777a20
faryadazim <faryadmaalic92@yahoo.com>
2022-02-01 01:50:52 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
-import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import { incNumber, decNumber } from "./Actions/index";
+import { useSelector ,useDispatch} from 'react-redux';
+import './App.css';
+import {incNumber,decNumber} from './actions/index'
 
-const App = () => {
-  const State = useSelector((state) => state.numberState);
-  const dispatch = useDispatch();
+
+function App() {
+const  myState = useSelector((state)=>state.changeNumber) 
+const dispatch = useDispatch()
   return (
-    <>
-      <div className="App">
-        <div className="App-header">
-          <h2>Redux Counter Application</h2>
-          <h4>Created By Faryad Azim</h4>
-          <div className="d-flex">
-            <span>
-              <button
-                className="px-4 mx-2 btn btn-danger my-2 w-25 btn-sm "
-                onClick={() => {
-                  dispatch(decNumber(2));
-                }}
-              >
-                -
-              </button>
-            </span>
-            <span>
-              <input
-                type="text"
-                className="form-control  form-control-sm my-2"
-                value={State}
-                style={{ textAlign: "center" }}
-              />
-            </span>
-            <span>
-              <button
-                onClick={() => {
-                  dispatch(incNumber(5));
-                }}
-                className="px-4 mx-2 btn btn-primary my-2 w-25 btn-sm d-inline"
-              >
-                +
-              </button>
-            </span>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+   
+   <h1>React Redux Counter Application</h1>
+   <h4> Faryad Azim </h4>
+
+<span> <button onClick={()=>{dispatch(incNumber())
+console.log('hello');}}> - </button>
+ <input style={{textAlign:'center'}} type="text" className="form-control" value={myState} /> 
+ <a href="/"> + </a></span>
+ 
+ 
+  
+ </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/Reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import numberState from "./counter";
-
-const rootReducer = combineReducers({numberState});
-export default rootReducer;
