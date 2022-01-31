5e51a358860066d3e7fdd4eef175b2ba5e6f4ec4
faryadazim <faryadmaalic92@yahoo.com>
2022-02-01 01:45:48 +0500

    All code revised and completed here we learn React Redux Basic to addvance consise features

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux'
-import store from './store'; 
-
- store.subscribe(()=>console.log(store.getState()));
-
+ import store from './store'
+import { Provider } from 'react-redux';
+//  store.subscribe(()=>console.log(store.getState))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+  <React.StrictMode> <Provider store={store}>
+    <App /> </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
-reportWebVitals();
+); 
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import changeNumber from "./upDown";
-const rootReducer=combineReducers({
-    changeNumber
-})
- 
-export default rootReducer;
