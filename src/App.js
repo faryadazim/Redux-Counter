1f82b20a72c6d41d73a12a834decc8dbb87b411a
faryadazim <faryadmaalic92@yahoo.com>
2022-01-31 05:19:44 +0500

    Basic UI done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+   <h1>React Redux Counter Application</h1>
+   <h4> Faryad Azim </h4>
+
+<span> <a href="/"> - </a> <input style={{textAlign:'center'}} type="text" className="form-control" value="0" /> <a href="/"> + </a></span>
+ 
+ 
+  
+ </header>
     </div>
   );
 }
